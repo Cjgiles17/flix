2b0951f5632f1acd2b7f9c60c18ce96391800edc
Caleb Giles <calebgiles@calebs-macbook-pro.local>
2019-02-05 22:36:08 -0500

    Part - 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Caleb Giles on 2/5/19.
+//  Copyright © 2019 Caleb Giles. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
